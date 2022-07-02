c563c268fca468176389fa2d98224fcb28747c95
Memereh <jeremy.vargas@outlook.com>
2022-07-02 11:22:06 -0400

    Crud de prueba

--- app/views.py
+++ app/views.py
@@ -27,3 +27,24 @@ class TipoProductoApi(APIView):
             tipoObj.descripcion = nombre
             tipoObj.save()
             return Response({"response":"Tipo producto guardado."})
+    def get(self,request):
+        lista = TIPO_PRODUCTO.objects.all().values()
+        return Response(lista)
+    def put(self,request):
+        if("id_tipo" in request.data):
+            id_tipo = request.data.get("id_tipo")
+            try:
+                tipoProducto = TIPO_PRODUCTO.objects.get(id_tipo_producto=id_tipo)
+            except TIPO_PRODUCTO.DoesNotExist:
+                return Response({"response":"Tipo de producto no existe"})
+            tipoProducto.descripcion = request.data.get("nombre")
+            tipoProducto.save()
+            return Response({"response":"Tipo producto Actualizado"})
+    def delete(self,request):
+        id_tipo = request.data.get("id_tipo")
+        try:
+            tipoProducto = TIPO_PRODUCTO.objects.get(id_tipo_producto = id_tipo)
+        except TIPO_PRODUCTO.DoesNotExist:
+            return Response({"response":"tipo de producto no existe"})
+        tipoProducto.delete()
+        return Response({"response":"tipo de producto eliminado correctamente"})
